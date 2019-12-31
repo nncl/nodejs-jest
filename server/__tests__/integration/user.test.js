8cc3cbdc8741041efc3eb9fc622048965328c993
= <nncl@live.com>
2019-12-31 18:43:12 -0300

    Testing user deuplicated e-mails creation

--- server/__tests__/integration/user.test.js
+++ server/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should NOT register duplicated e-mails', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Caue',
+        email: 'caue@almeida.com',
+        password_hash: '123',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Caue',
+        email: 'caue@almeida.com',
+        password_hash: '123',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
--- server/src/app/controllers/UserController.js
+++ server/src/app/controllers/UserController.js
@@ -2,6 +2,13 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const { email } = req.body;
+    const checkUser = await User.findOne({ where: { email } });
+
+    if (checkUser) {
+      return res.status(400).json({ error: 'User already registered' });
+    }
+
     const user = await User.create(req.body);
     return res.json(user);
   }
